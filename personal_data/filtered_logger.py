525274536c72d2cbd81b624db0a26d3e38af9e6d
barasa254 <101549153+barasa001@users.noreply.github.com>
2024-01-23 21:30:39 +0300

    update

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -16,26 +16,6 @@
 ''' 0. Regex-ing '''
 
 import re
-from typing import List
 
-
-def filter_datum(fields: List[str], redaction: str, message: str,
-                 separator: str) -> str:
-    ''' Return `message` with personally identifiable information redacted. '''
-    ''' Description: Regex-ing - Write a function called filter_datum that
-                     returns the log message obfuscated:
-        Arguments:
-            fields: a list of strings representing all fields to obfuscate
-            redaction: a string representing by what the field will be
-                       obfuscated
-            message: a string representing the log line
-            separator: a string representing by which character is
-                           separating all fields in the log line (message)
-        The function should use a regex to replace occurrences of certain
-        field values.
-        filter_datum should be less than 5 lines long and use re.sub to
-        perform the substitution with a single regex.
-    '''
-
-    for i in fields:
-        message = re.sub(i + "=.*?" + separator,
+def filter_datum(fields, redaction, message, separator):
+    return re.sub(fr'(?<=\{separator}|^)({"|".join(fields)})=\S+?(?=\{separator}|$)', f'{redaction}', message)
