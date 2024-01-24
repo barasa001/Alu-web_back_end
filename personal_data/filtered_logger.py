9cb110a3379ecbccc987ddd98b5d7f21d5d2ea8c
barasa254 <101549153+barasa001@users.noreply.github.com>
2024-01-24 13:37:54 +0300

    indentation

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -13,15 +13,14 @@ def filter_datum(fields: List[str], redaction: str,
         message = re.sub(f'{f}=.*?{separator}',
                          f'{f}={redaction}{separator}', message)
     return message
-                   
+
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
+    """ Redacting Formatter class """
+
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self):
     def __init__(self, fields: List[str]):
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
