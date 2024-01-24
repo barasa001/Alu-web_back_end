c309e1fb8f7dff89fb7202e663d84358217cdc57
barasa001 <s.wanyonyi@alustudent.com>
2024-01-24 11:33:22 +0000

    added whitespaces

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -3,6 +3,8 @@
 Encrypting passwords
 """
 import bcrypt
+
+
 def hash_password(password: str) -> bytes:
     """ Returns a salted, hashed password, which is a byte string """
     encoded = password.encode()
