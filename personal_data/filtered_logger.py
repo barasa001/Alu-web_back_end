f3728ee32fac1760059fdb951f496f15ecb162a2
barasa254 <101549153+barasa001@users.noreply.github.com>
2024-01-24 14:20:50 +0300

    next file

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,49 +1,42 @@
 #!/usr/bin/env python3
 """ Use of regex in replacing occurrences of certain field values """
 import re
-import logging
 from typing import List
-'''
-import re
 import logging
-from typing import List
-from os import environ
-from mysql.connector import connection
-'''
-PII_FIELDS = ("name", "email", "phone", "ssn", "password")
-
-
+import mysql.connector
+import os
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
     """
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
-
     def __init__(self, fields: List[str]):
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
-
     def format(self, record: logging.LogRecord) -> str:
         """ Returns filtered values from log records """
         return filter_datum(self.fields, self.REDACTION,
                             super().format(record), self.SEPARATOR)
-
-
-'''
-def get_db() -> connection.MYSQLConnection:
+PII_FIELDS = ("name", "email", "password", "ssn", "phone")
+def get_db() -> mysql.connector.connection.MYSQLConnection:
     """ Connection to MySQL environment """
-    username = environ.get("PERSONAL_DATA_DB_USERNAME", "root")
-    password = environ.get("PERSONAL_DATA_DB_PASSWORD", "")
-    db_host = environ.get("PERSONAL_DATA_DB_HOST", "localhost")
-    db_name = environ.get("PERSONAL_DATA_DB_NAME")
-    connector = connection.MySQLConnection(
-        user=username,
-        password=password,
-        host=db_host,
-        database=db_name)
-    return connector
-'''
+    db_connect = mysql.connector.connect(
+        user=os.getenv('PERSONAL_DATA_DB_USERNAME', 'root'),
+        password=os.getenv('PERSONAL_DATA_DB_PASSWORD', ''),
+        host=os.getenv('PERSONAL_DATA_DB_HOST', 'localhost'),
+        database=os.getenv('PERSONAL_DATA_DB_NAME')
+    )
+    return db_connect
+
+
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """ Returns regex obfuscated log messages """
+    for field in fields:
+        message = re.sub(f'{field}=(.*?){separator}',
+                         f'{field}={redaction}{separator}', message)
+    return message
 
 
 def get_logger() -> logging.Logger:
@@ -51,22 +44,16 @@ def get_logger() -> logging.Logger:
     logger = logging.getLogger("user_data")
     logger.setLevel(logging.INFO)
     logger.propagate = False
-
     target_handler = logging.StreamHandler()
-    target_handler.setFormatter(RedactingFormatter(PII_FIELDS))
-    logger.addHandler(target_handler)
     target_handler.setLevel(logging.INFO)
-
     formatter = RedactingFormatter(list(PII_FIELDS))
     target_handle.setFormatter(formatter)
-
     logger.addHandler(target_handler)
     return logger
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
-
     """ Returns regex obfuscated log messages """
     for field in fields:
         message = re.sub(f'{field}=(.*?){separator}',
