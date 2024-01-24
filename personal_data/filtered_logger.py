f4f63199a3cead752da94308ac51c9b2daf08287
barasa001 <s.wanyonyi@alustudent.com>
2024-01-24 11:18:55 +0000

    whitespace

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -43,9 +43,9 @@ def get_db() -> connection.MYSQLConnection:
         host=db_host,
         database=db_name)
     return connector
-
-
 '''
+
+
 def get_logger() -> logging.Logger:
     """ Returns a logging.Logger object """
     logger = logging.getLogger("user_data")
